da1e9f3c9175da469b95f4240ea240403ea29c64
upluke <lukeforadream@gmail.com>
2023-07-22 10:12:45 -0700

    added two panels to Game

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 /**
  * @author Aiyana Arnobit
@@ -8,7 +9,21 @@ import javax.swing.JFrame;
  */
 
 public class Game extends JFrame{
+    public Game(){
+        super("Spider Game");
+        setLayout(new BorderLayout());
+
+        WorldPanel worldPanel = new WorldPanel();
+        WorkAreaPanel workAreaPanel = new WorkAreaPanel();
+
+        add(worldPanel, BorderLayout.WEST);
+        add(workAreaPanel, BorderLayout.CENTER);
+    }
     public static void main (String[] args) {
-        
+        Game win = new Game();
+        win.setSize(1400, 800);
+        win.setLocationRelativeTo(null);
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.setVisible(true);
     }
 }
