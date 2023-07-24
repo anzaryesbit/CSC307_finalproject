a7df94664bf260a6554ffc2fc609976eaf3dec4b
Briana Kuo <brkuo@calpoly.edu>
2023-07-23 21:33:17 -0700

    painting blocks a certain color

--- Block.java
+++ Block.java
@@ -6,7 +6,12 @@
  */
 
 public class Block extends ParentBlock {
+    char paintColor = 0;
+
     Block(int x, int y, String type) {
         super(x, y, type);
     }
+
+    public void setPaintColor(char c) { this.paintColor = c; }
+    public char getPaintColor() { return this.paintColor; }
 }
