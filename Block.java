fe35bd70d02c6c915576520534984e4c73ec2687
upluke <lukeforadream@gmail.com>
2023-07-26 11:37:28 -0700

    added id attribute to Block

--- Block.java
+++ Block.java
@@ -7,11 +7,25 @@
 
 public class Block extends ParentBlock {
     char paintColor = 0;
+    private int id;
 
-    Block(int x, int y, String type) {
-        super(x, y, type);
+    Block(int x, int y, String type) {super(x, y, type);}
+
+    Block(int x, int y, String type, int id){
+        this(x, y, type);
+        this.id = id;
     }
 
     public void setPaintColor(char c) { this.paintColor = c; }
     public char getPaintColor() { return this.paintColor; }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+
 }
